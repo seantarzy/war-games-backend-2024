91c88813583eea157ba2ecb0406025784437a22f
Sean Tarzy <seantarzy@seans-mbp.mynetworksettings.com>
2024-03-24 15:56:24 -0400

    delete /

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [
     'https://war-games-2-0-api-red-tree-4407.fly.dev',
     'http://localhost:5173',
-    'https://main--war-games.netlify.app/',
+    'https://main--war-games.netlify.app',
     # Add any other origins you want to allow
   ]
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
