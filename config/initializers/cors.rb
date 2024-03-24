7aa4d2b9d74cf008ed72daca8a444cc8a303c813
Sean Tarzy <seantarzy@seans-mbp.mynetworksettings.com>
2024-03-24 16:06:40 -0400

    cable + server whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:5173"
+    origins "http://localhost:3000", "http://localhost:5173", "https://main--war-games.netlify.app"
 
     resource "*",
       headers: :any,
