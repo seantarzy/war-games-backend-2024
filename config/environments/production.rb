1105bdcadbb3cd3abee1b8d85b82626f74b2a0f8
Sean Tarzy <seantarzy@Seans-MBP.attlocal.net>
2024-03-31 23:46:56 -0500

    whitelist

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
     'https://war-games-2-0-api-red-tree-4407.fly.dev',
     'https://immense-temple-53760-309cb2c4c494.herokuapp.com/',
     'https://main--war-games.netlify.app',
+    'https://baberuth.app',
+    'https://www.baberuth.app',
     # Add any other origins you want to allow
   ]
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:5173", "https://main--war-games.netlify.app"
+    origins "http://localhost:3000", "http://localhost:5173", "https://main--war-games.netlify.app", "https://baberuth.app", "https://www.baberuth.app", "https://immense-temple-53760-309cb2c4c494.herokuapp.com", "https://war-games-2-0-api-red-tree-4407.fly.dev"
 
     resource "*",
       headers: :any,
